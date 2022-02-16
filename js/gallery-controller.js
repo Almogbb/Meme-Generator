df1dcf118e7472faa05be1064f862e0cb72c2a77
Almogbb <98409854+Almogbb@users.noreply.github.com>
2022-02-16 19:53:43 +0200

    fixing img.....

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -5,7 +5,7 @@ function renderImages() {
     const images = getImages();
     let strHTML = images.map(image => {
         return `<div class="meme-option">
-        <img data-img="${image.id}" class="image image${image.id}" src="../meme-imgs-square/${image.url}.JPG" onclick="onImgClick(this)">
+        <img data-img="${image.id}" class="image image${image.id}" src="./meme-imgs-square/${image.url}.JPG" onclick="onImgClick(this)">
         </div>`
     })
     const elDiv = document.querySelector('.meme-images');
